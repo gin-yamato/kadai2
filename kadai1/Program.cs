62a6e83ef776a6774c5ff275193c585ed25dd04f
gin-yamato <68271056+gin-yamato@users.noreply.github.com>
2020-09-02 20:28:22 +0900

    課題です！1年_A2　須藤
    自分が設定した文字を、
    再度打ち直すまで繰り返すプログラムです。
    工夫したところは、文字を間違えた場合に残る
    文字を消すところと時間表示をできるように
    したところです。

---
+++ kadai1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Transactions;
+
+namespace kadai1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("passコードを入力してください。");
+            string userPass = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("passコードの設定が完了しました。");
+
+            string correctAnswer = userPass;
+            Console.WriteLine("passコード入力");
+            string userAnswer = Console.ReadLine();
+
+
+            while (userAnswer != correctAnswer)
+            {
+                
+                Console.Clear();
+                Console.WriteLine("入力が不正です");
+
+                DateTime dt = DateTime.Now;
+                Console.WriteLine(dt);
+
+                Console.WriteLine("passコード入力してください");
+                userAnswer = Console.ReadLine();
+            }
+            Console.WriteLine("passコードを認証しました");
+            Console.ReadKey();
+           
+        }
+        
+    }
+}
+
+
+
+
